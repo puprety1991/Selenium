c92dced7514c311620629844cce60c4f3a0d9f01
Puskar Uprety <upretypuskar@gmail.com>
2022-10-18 20:13:29 -0400

    Intro

---
+++ class01/LaunchBrowser.java
@@ -0,0 +1,18 @@
+package com.syntax.class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.file.Watchable;
+
+public class LaunchBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://google.com");
+        driver.manage().window().maximize();
+        driver.navigate().to("http://facebook.com");
+        Thread.sleep(100);
+        driver.quit();
+    }
+}
