5bb7d3614f1976aaf81c753f4ceb7760361a49f3
Puskar Uprety <upretypuskar@gmail.com>
2022-10-18 20:57:37 -0400

    launch browser

--- class01/class01.java
+++ class01/class01.java
@@ -5,17 +5,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class class01 {
     public static void main(String[] args) {
+        //set the path to the driver to the link it with our class
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        //create WebDriver instance
         WebDriver driver= new ChromeDriver();
+        // use get() function to open up the required website
         driver.get("http://google.com");
+        //get the url
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        //get the title
         String title = driver.getTitle();
         System.out.println(title);
+        //use equal method to check its condition
         if(title.equals("Google")){
             System.out.println("This is the right title");
         }else{
             System.out.println("Wrong title");
         }
+        //close the browser
         driver.quit();
 
     }
 }
+
