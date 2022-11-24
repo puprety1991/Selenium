266ce6e644f0280185263022fa5746c7cf30b991
Puskar Uprety <upretypuskar@gmail.com>
2022-11-23 21:24:59 -0500

    new concepts

--- class12/jsExecutorHighlight.java
+++ class12/jsExecutorHighlight.java
@@ -1,28 +1,26 @@
 package com.syntax.class12;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.syntax.utils.CommonMethods.teardown;
-
-public class jsExecutorScroll {
+public class jsExecutorHighlight {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login%E2%80%9D");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        //scroll down
 
+        WebElement userNameField = driver.findElement(By.id("txtUsername"));
+        userNameField.sendKeys("Admin");
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,3000)");
-        js.executeScript("window.scrollBy(0,-3000)");
+        js.executeScript("arguments[0].style.backgroundColor='pink'",userNameField);
 
 
-       // teardown();
     }
 }
--- class12/uploadFile.java
+++ class12/uploadFile.java
@@ -1,28 +1,23 @@
 package com.syntax.class12;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.syntax.utils.CommonMethods.teardown;
-
-public class jsExecutorScroll {
+public class uploadFile {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-
+        driver.get("https://the-internet.herokuapp.com/upload");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        //scroll down
+        WebElement chooseFile = driver.findElement(By.xpath("//input[@id='file-upload']"));
+        chooseFile.sendKeys("/Users/puskaruprety/Desktop/image1.jpeg");
+        WebElement uploadBtn = driver.findElement(By.id("file-submit"));
+        uploadBtn.click();
 
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,3000)");
-        js.executeScript("window.scrollBy(0,-3000)");
-
-
-       // teardown();
     }
 }
